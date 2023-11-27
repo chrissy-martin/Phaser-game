e08d36f14b99353dfb0b70f72cb60c600e702b5a
C Martin <cmart131@yahoo.com>
2023-11-27 15:30:25 -0800

    added comments into the code

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
+//add create() function. create() sets up all of the game objects to set stage.
 function create() {
     this.add.text(50, 50, 'Critters of Chaos');
 }
-
+//config object specifies some meta-information about the game
 const config = {
     width: 500,
     height: 600 ,
     backgroundColor: 0x124D3E,
+// add in scene information
     scene: {create}
   }
   
