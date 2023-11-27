45d3e0c73fcf962ddea46365308d5c7fbfc99b4f
C Martin <cmart131@yahoo.com>
2023-11-27 15:24:11 -0800

    added create function and scene property in config object.

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
+function create() {
+    this.add.text(50, 50, 'Critters of Chaos');
+}
+
 const config = {
     width: 500,
     height: 600 ,
-    backgroundColor: 0x124D3E
+    backgroundColor: 0x124D3E,
+    scene: {create}
   }
   
   const game = new Phaser.Game(config);
